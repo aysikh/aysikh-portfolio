9aa2dd332334c053205c874e10aedfaf9e840516
aysikh <amy.sikhammountry@gmail.com>
2020-12-20 00:02:45 -0500

    added route to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import { NavLink, Redirect, Link } from 'react-router-dom'
 
 const useStyles = makeStyles({
   root: {
@@ -32,9 +33,9 @@ export default function CenteredTabs() {
         textColor="secondary"
         centered
         >
-        <Tab label="About Me" />
-        <Tab label="Projects" />
-        <Tab label="Contact Me" />
+        <Tab label="About Me" to='/aboutme' component={Link}/>
+        <Tab label="Projects" to='/projects' component={Link}/>
+        <Tab label="Contact Me" to='/contact' component={Link}/>
       </Tabs>
     </Paper>
         </div>
