3f1d86a554cc7f0fb61f417eba862330db76b6bc
aysikh <amy.sikhammountry@gmail.com>
2020-12-24 23:52:55 -0500

    adding to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,40 @@ import Hidden from '@material-ui/core/Hidden';
 export default function Project() {
   return(
     <div>
-    projects here
+     <div className='ProjectsCard'>
+      <div className='ProjectsCard-left'>
+        <ProgressiveImage src={image} placeholder={mini}>
+          {(src, loading) => (
+            <img
+              style={{
+                opacity: loading ? 0.7 : 1,
+              }}
+              src={src}
+              alt='project'
+            />
+          )}
+        </ProgressiveImage>
+        <div className='ProjectsCard-left-info'>
+          <span className='ProjectsCard-left-info-left'>type</span>
+          <span className='ProjectsCard-left-info-right' style={{ color }}>
+            {type}
+          </span>
+          <span className='ProjectsCard-left-info-left'>stack</span>
+          <span className='ProjectsCard-left-info-right' style={{ color }}>
+            {stack}
+          </span>
+        </div>
+        <div className='ProjectsCard-left-buttons'>{renderLinks()}</div>
+      </div>
+      <div
+        className='ProjectsCard-center'
+        style={{ backgroundColor: color }}
+      ></div>
+      <div className='ProjectsCard-right'>
+        <span className='ProjectsCard-right-header'>{name}</span>
+        <p>{description}</p>
+      </div>
+    </div>
     </div>
   )
 }
