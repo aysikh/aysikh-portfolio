1bd082c7c673b8dd82924d76b556ecbc9396007a
aysikh <amy.sikhammountry@gmail.com>
2021-01-07 22:34:50 -0500

    realigned about me box with grid

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,6 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import ProfilePic from '../assets/profilepic.jpg'
 import { CssBaseline } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 
 import Email from '../assets/email.png'
 import Github from '../assets/github.png'
@@ -12,16 +13,22 @@ import Medium from '../assets/medium.png'
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: 645,
-    background: 'rgba(0,0,0,0.5)',
-    margin: '20px',
+    width: '80rem',
+    // background: 'rgba(0,0,0,0.5)',
+    // margin: '20px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    boxShadow: 'none',
+    margin: '0',
+    border: '0',
+    overflowX: 'hidden'
   },
   media: {
     height: "35rem",
-    width: "30rem"
+    width: "35rem"
   },
   stuff: {
-    // minHeight: '100vh',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
@@ -32,9 +39,6 @@ const useStyles = makeStyles((theme) => ({
   icons: {
     height: '5rem'
   },
-  // iconsdiv: {
-  //   marginLeft: '10rem'
-  // }
 
 }));
 
@@ -43,35 +47,37 @@ export default function AboutMe({ checked }) {
 
   return (
     <div>
-      <div>
-        <div id="social-links-div" className={classes.iconsdiv}>
-          <a href={`mailto:amy.sikhammountry@gmail.com`} >
-            <img src={Email} alt='' className={classes.icons} />
-          </a><br></br>
-          <a href="https://github.com/aysikh" rel="noopener noreferrer" target="_blank" >
-            <img src={Github} alt='' className={classes.icons} />
-          </a><br></br>
-          <a href="https://www.linkedin.com/in/aysikh" rel="noopener noreferrer" target="_blank" >
-            <img src={Linkedin} alt='' className={classes.icons} />
-          </a><br></br>
-          <a href="https://medium.com/aysikh" rel="noopener noreferrer" target="_blank" >
-            <img src={Medium} alt='' className={classes.icons} />
-          </a>
-        </div>
-        <div>
-            {/* <div className={classes.root}> */}
-                <Card checked={checked}>
-                    <CardMedia
-                    className={classes.media}
-                    image={ProfilePic}
-                    title="Profile Picture"
-                    />
-                </Card>
-                <Card >
-                    just a scrub doing scrub things
-                </Card>
-                {/* </div> */}
-        </div>
+      <div className={classes.root} id="about-me">
+        <Grid item xs={ 3 } >
+          <div id="social-links-div" className={classes.iconsdiv}>
+            <a href={`mailto:amy.sikhammountry@gmail.com`} >
+              <img src={Email} alt='' className={classes.icons} />
+            </a><br></br>
+            <a href="https://github.com/aysikh" rel="noopener noreferrer" target="_blank" >
+              <img src={Github} alt='' className={classes.icons} />
+            </a><br></br>
+            <a href="https://www.linkedin.com/in/aysikh" rel="noopener noreferrer" target="_blank" >
+              <img src={Linkedin} alt='' className={classes.icons} />
+            </a><br></br>
+            <a href="https://medium.com/aysikh" rel="noopener noreferrer" target="_blank" >
+              <img src={Medium} alt='' className={classes.icons} />
+            </a>
+          </div>
+        </Grid>
+        <Grid item xs={ 5 } style={{margin: '2rem'}}>
+          <Card checked={checked}>
+              <CardMedia
+              className={classes.media}
+              image={ProfilePic}
+              title="Profile Picture"
+              />
+          </Card>
+        </Grid>
+        <Grid item xs={ 5 } > 
+          <Card style={{padding: '3rem'}}>
+            Before my interest in programming, I was a full-time personal trainer and bartender on the side. Fitness and health was and is still a huge factor in my lifestyle. When the pandemic occurred, it helped me realize that I had to make a huge decision for myself. I wanted to continue training, but I was also concerned about my family’s well-being. I sought out the path of becoming a programmer to have the chance to provide for my family in case another pandemic were to occur in the future. I want to have a higher probability of keeping my job and continue supporting my family. In the process, I fell in love with programming and the endless amount of material I could possibly learn. Microstrategy is a company I want to work for because of the doors it could open up. It is a company that promotes diversity, innovation, and growth. I am very enthusiastic to continue learning and emerge in new experiences that Microstrategy has to offer. 
+          </Card>
+        </Grid>
       </div>
     </div>
   );
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -19,109 +19,109 @@ import Reacticon from '../assets/react.png'
 import Ruby from '../assets/ruby.png'
 import Sass from '../assets/sass.png'
 
-    const useStyles = makeStyles((theme) => ({
-        root: {
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-            boxShadow: 'none',
-            margin: '0',
-            border: '0',
-            overflowX: 'hidden'
-        },
-        icon: {
-            color: 'black',
-            fontSize: '2rem',
-        },
-        colorText: {
-            color: '#1a904b',
-        },
-        container: {
-            textAlign: 'center',
-        },
-        title: {
-            color: 'black',
-            fontSize: '4.5rem',
-        },
-        div:{
-            margin: '0',
-            border: '0'
-        },
-        icons:{
-          height: '5rem'
-        }
-        }));
+  const useStyles = makeStyles((theme) => ({
+      root: {
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          boxShadow: 'none',
+          margin: '0',
+          border: '0',
+          overflowX: 'hidden'
+      },
+      icon: {
+          color: 'black',
+          fontSize: '2rem',
+      },
+      colorText: {
+          color: '#1a904b',
+      },
+      container: {
+          textAlign: 'center',
+      },
+      title: {
+          color: 'black',
+          fontSize: '4.5rem',
+      },
+      div:{
+          margin: '0',
+          border: '0'
+      },
+      icons:{
+        height: '4.5rem'
+      }
+      }));
 
-    export default function Header() {
-        const classes = useStyles();
-        const [checked, setChecked] = useState(false);
-        useEffect(() => {
-            setChecked(true);
-        }, []);
+  export default function Header() {
+      const classes = useStyles();
+      const [checked, setChecked] = useState(false);
+      useEffect(() => {
+          setChecked(true);
+      }, []);
 
 
-    return (
-        <div className={classes.div}>
-          <div className={classes.root} id="welcome">
-            {/* <Grid container spacing={ 16 } style={ { padding: '5rem', marginTop: '8rem' } }> */}
-              <Grid item xs={ 1 }>
-                <Collapse
+  return (
+      <div className={classes.div}>
+        <div className={classes.root} id="welcome">
+          {/* <Grid container spacing={ 16 } style={ { padding: '5rem', marginTop: '8rem' } }> */}
+            <Grid item xs={ 1 }>
+              <Collapse
+              in={checked}
+              {...(checked ? {timeout: 2000 } : {})}
+              // collapsedHeight={50}
+              >
+                <div className={classes.container} id="icons"> 
+                  <img src={Ruby} alt='ruby' className={classes.icons} />
+                  <br/>
+                  <img src={Rails} alt='rubyonrails' className={classes.icons} />
+                  <br/>
+                  <img src={JS} alt='javascript' className={classes.icons} />
+                  <br/>
+                  <img src={Reacticon} alt='react' className={classes.icons} />
+                  <br/>
+                  <img src={HTML} alt='react' className={classes.icons} />
+                  <br/>
+                  <img src={CSS} alt='react' className={classes.icons} />
+                  <br/>
+                  <img src={Bootstrap} alt='react' className={classes.icons} />
+                  <br/>
+                  <img src={Material} alt='react' className={classes.icons} />
+                  <br/>
+                  <img src={Sass} alt='react' className={classes.icons} />
+                  <br/>
+                  <img src={Heroku} alt='react' className={classes.icons} />
+                  <br/>
+                </div>
+              </Collapse>
+            </Grid>
+            <Grid item xs={ 8 } style= {{marginTop: '10rem', marginLeft: '-5rem'}}>
+              <Fade
                 in={checked}
-                {...(checked ? {timeout: 2000 } : {})}
-                // collapsedHeight={50}
+                {...(checked ? { timeout: 2000 } : {})}
+                collapsedHeight={50}
                 >
-                  <div className={classes.container} id="icons"> 
-                    <img src={Ruby} alt='ruby' className={classes.icons} />
-                    <br/>
-                    <img src={Rails} alt='rubyonrails' className={classes.icons} />
-                    <br/>
-                    <img src={JS} alt='javascript' className={classes.icons} />
-                    <br/>
-                    <img src={Reacticon} alt='react' className={classes.icons} />
-                    <br/>
-                    <img src={HTML} alt='react' className={classes.icons} />
-                    <br/>
-                    <img src={CSS} alt='react' className={classes.icons} />
-                    <br/>
-                    <img src={Bootstrap} alt='react' className={classes.icons} />
-                    <br/>
-                    <img src={Material} alt='react' className={classes.icons} />
-                    <br/>
-                    <img src={Sass} alt='react' className={classes.icons} />
-                    <br/>
-                    <img src={Heroku} alt='react' className={classes.icons} />
-                    <br/>
+                  <div className={classes.container}>
+                    <h1 className={classes.title}>
+                        <span style={{color: '#f8bbd0'}}>amy sikhammountry</span>
+                        <br/> 
+                        full-stack developer
+                    </h1>
+                    <h3>learn more about me below </h3>
+
+                    <Scroll to="about me" smooth={true}>
+                        <IconButton>
+                            <KeyboardArrowDownIcon style={{color: '#c48b9f', fontSize: '5rem'}} />
+                        </IconButton>
+                    </Scroll>
                   </div>
-                </Collapse>
-              </Grid>
-              <Grid item xs={ 8 } style= {{marginTop: '10rem', marginLeft: '-5rem'}}>
-                <Fade
-                  in={checked}
-                  {...(checked ? { timeout: 2000 } : {})}
-                  collapsedHeight={50}
-                  >
-                    <div className={classes.container}>
-                      <h1 className={classes.title}>
-                          <span style={{color: '#f8bbd0'}}>amy sikhammountry</span>
-                          <br/> 
-                          full-stack developer
-                      </h1>
-                      <h3>learn more about me below </h3>
+              </Fade>
+            </Grid>
+          {/* </Grid> */}
+          </div>
+          <AboutMeContainer />
 
-                      <Scroll to="about me" smooth={true}>
-                          <IconButton>
-                              <KeyboardArrowDownIcon style={{color: '#c48b9f', fontSize: '5rem'}} />
-                          </IconButton>
-                      </Scroll>
-                    </div>
-                </Fade>
-              </Grid>
-            {/* </Grid> */}
-            </div>
-            <AboutMeContainer />
-
-        </div>
-    );
-    }
+      </div>
+  );
+  }
 
