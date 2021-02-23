0826396b9b0aae54132ef064f1da335ecc843e1c
aysikh <amy.sikhammountry@gmail.com>
2021-02-22 21:34:31 -0500

    added form for contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,42 @@
 import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button'
 
 export default function Contact(){ 
-  
+
   return (
     <div>
-      Contact me here 
+      <Paper elevation={3}>
+                  <form autoComplete="on">
+                    <TextField 
+                      fullWidth
+                      id="outlined-basic" 
+                      label="Title" 
+                      variant="outlined" 
+                      />
+                      <br /> <br />
+                      
+                    <TextField
+                      fullWidth
+                      id="outlined-multiline-static"
+                      label="Your response"
+                      multiline
+                      rows={30}
+                      variant="outlined"
+                      
+                    />
+                      <br/> <br/> 
+                      <Button 
+                        type="submit" 
+                        variant="contained" 
+                        color="primary">
+                        Submit
+                      </Button> 
+                    </form>
+                </Paper>
     </div>
   )
 }
